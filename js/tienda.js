f0e0fcc7f040a5d1b6d927ffb0557ed537beffb1
Hector Jair Velazquez Jimenez <a340389707@my.uvm.edu.mx>
2024-03-11 20:03:17 -0600

    filtro de ordenar por nombre asc y desc

--- js/tienda.js
+++ js/tienda.js
@@ -5,15 +5,16 @@ function cargarTienda() {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
       const datos = JSON.parse(xhr.responseText);
-      const productos = datos.Productos;
-      var contenedor = document.getElementById("tnd");
+      var productos = datos.Productos;
+      productos = ordenarTitulo(productos, 0);
+      const contenedor = document.getElementById("tnd");
       for (let i = 0; i < productos.length; i++) {
         var item = document.createElement("div");
         item.className = "Tienda-item";
         var titulo = document.createElement("H2");
         titulo.innerHTML = productos[i].Titulo;
         var sub = document.createElement("h5");
-        sub.innerHTML = productos[i].Sub;
+        sub.innerHTML = "$" + productos[i].Sub + " MXN";
         var img = document.createElement("img");
         img.className = "Producto-IMG";
         img.src = productos[i].Ruta;
@@ -26,4 +27,30 @@ function cargarTienda() {
     }
   };
   xhr.send();
+}
+function ordenarTitulo(productos, orden) {
+  if (orden === 1) {
+    productos.sort((a, b) => {
+      const ta = a.Titulo.toLowerCase();
+      const tb = b.Titulo.toLowerCase();
+      if (ta < tb) {
+        return -1;
+      } else if (ta > tb) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (orden === -1) {
+    productos.sort((a, b) => {
+      const ta = a.Titulo.toLowerCase();
+      const tb = b.Titulo.toLowerCase();
+      if (ta < tb) {
+        return 1;
+      } else if (ta > tb) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+  return productos;
 }
