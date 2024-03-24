3a5b766a386e902f95dab480f033d50ac1879758
Hector Jair Velazquez Jimenez <a340389707@my.uvm.edu.mx>
2024-03-24 16:16:31 -0600

    api

--- BeautyShopMX/js/config.js
+++ BeautyShopMX/js/config.js
@@ -1,7 +1,9 @@
 // modulo.js
 const api = (function () {
   // Variables privadas
-  let url = "http://localhost:3977/api/v1";
+  //let url = "http://localhost:3977/api/v1";
+  let url = "http://102.168.1.76:3977/api/v1";
+
   // Lo que deseas exponer públicamente
   return {
     getUrl: function () {
--- server/constants.js
+++ server/constants.js
@@ -3,7 +3,8 @@ const DB_PASSWORD = "admin123456";
 const DB_HOST = "beauty-shop.rdwjngs.mongodb.net";
 
 const API_VERSION = "v1";
-const IP_SERVER = "localhost";
+//const IP_SERVER = "localhost";
+const IP_SERVER = "192.168.1.76";
 const JWT_SECRET_KEY = "D7#fn!GpL&8sUz@Q5Yj2gX1aRvO4wEhNcVbIuT9y6kJmP3qKHr4F";
 module.exports = {
   DB_USER,
