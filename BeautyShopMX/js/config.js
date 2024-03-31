729cc4054181782dc0ed9b4e6c53c4a52ec0007b
Hector Jair Velazquez Jimenez <a340389707@my.uvm.edu.mx>
2024-03-31 00:39:43 -0600

    update config

--- BeautyShopMX/js/config.js
+++ BeautyShopMX/js/config.js
@@ -2,8 +2,8 @@
 const api = (function () {
   // Variables privadas
   //let url = "http://localhost:3977/api/v1";
-  let url = "http://localhost:3977/api/v1";
-  let urlIMG = "http://localhost:3977/";
+  let url = "http://servergutierrez.ddns.net/:3977/api/v1";
+  let urlIMG = "http://servergutierrez.ddns.net/";
   const categorias = [
     "Todos",
     "Ropa",
